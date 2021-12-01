3b34ba4252f5e113b6dfb1b1bc8fade39bd20c77
Marcos Gouveia <mgo.fis@protonmail.com>
2021-12-01 12:50:18 +0000

    minor changes to day1

--- day1/day1.c
+++ day1/day1.c
@@ -2,31 +2,30 @@
 //	1607429-20211201-a1c1781e
 int partA() {
 	FILE *infile = fopen("input","r");
-	int n1,n2,res;
-	res = 0;
+	int n1,n2,res = 0;
 	fscanf(infile,"%d",&n1);
 	while(fscanf(infile,"%d",&n2)!=EOF) {
-		res += (n2>n1)?1:0;
+		res += (n2>n1);
 		n1=n2;
 	}
+	fclose(infile);
 	return res;
 }
 
 int partB() {
 	FILE *infile = fopen("input","r");
-	int n1,n2,n3,n4,res;
-	res = 0;
-	fscanf(infile,"%d",&n1);
-	fscanf(infile,"%d",&n2);
-	fscanf(infile,"%d",&n3);
+	int n1,n2,n3,n4,res = 0;
+	fscanf(infile,"%d\n%d\n%d",&n1,&n2,&n3);
 	while(fscanf(infile,"%d",&n4)!=EOF) {
-		res+=n4>n1?1:0;
+		res += n4>n1;
 		n1=n2;n2=n3;n3=n4;
 	}
+	fclose(infile);
 	return res;
 }
 
 int main() {
 	printf("%d\n",partA());
 	printf("%d\n",partB());
+	return 0;
 }
