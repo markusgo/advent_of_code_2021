74273868dc8ab84148bffd34c31077214ecd28e0
Marcos Gouveia <mgo.fis@protonmail.com>
2021-12-05 17:59:21 +0000

    day5 more compact

--- day5/day5.py
+++ day5/day5.py
@@ -1,39 +1,22 @@
 import numpy as np
-data = [i.strip().replace("->","").replace(","," ").split() for i in open("input","r") ]
-maxx=0
-maxy=0
-for i in range(len(data)):
-	for j in range(len(data[i])):
-		data[i][j] = int(data[i][j])
-	if max((data[i][0],data[i][2]))>maxx:
-		maxx = max((data[i][0],data[i][2]))
-	if max((data[i][1],data[i][3]))>maxy:
-		maxy = max((data[i][1],data[i][3]))
+data = np.array([[int(j) for j in (i.strip().replace("->","").replace(","," ").split())] for i in open("input","r")])
+maxx = np.max([np.max(data[:,0]),np.max(data[:,2])])
+maxy = np.max([np.max(data[:,1]),np.max(data[:,2])])
 print(maxx,maxy)
 crosses = np.zeros((maxx+1,maxy+1))
 for i in data:
-	start = i[0:2]
-	end = i[2:4]
-	vec=[0,0]
-	if start[0]>end[0]:
-		vec[0] = -1
-	elif start[0]<end[0]:
-		vec[0] = 1
-	else:
-		vec[0] = 0
-
-	if start[1]>end[1]:
-		vec[1] = -1
-	elif start[1]<end[1]:
-		vec[1] = 1
-	else:
-		vec[1] = 0
+	start = np.array(i[0:2])
+	end = np.array(i[2:4])
+	vec=np.sign(end-start)
+	
+	#Comment out for Part 2
+	if np.prod(vec):
+		continue
+	#----------------------
 	sent=1
 	while sent:
 		if start[0]==end[0] and start[1]==end[1]:
 			sent=0
 		crosses[start[0],start[1]]+=1
-		start[0]+=vec[0]
-		start[1]+=vec[1]
-
+		start+=vec
 print(np.sum(np.where(crosses>=2,1,0)))
