ef0745e05d870e17a784782f0bc9934796720796
Marcos Gouveia <marcosgouveia13@gmail.com>
2021-12-06 16:00:28 +0000

    day 6 completed

--- day6/day6.py
+++ day6/day6.py
@@ -1,12 +1,14 @@
-fish = [int(i) for i in open("input","r").read().strip().split(",")]
-day=0
-while day<80:
-	day+=1
-	toadd = 0
-	for j in range(len(fish)):
-		fish[j] = fish[j]-1
-		if fish[j]==0:
-			toadd+=1
-			fish[j]=7
-	fish.append([9 for i in range(toadd)])
-print(len(fish))
+import numpy as np
+from collections import Counter
+fish  = [int(i) for i in open("input","r").read().strip().split(",")]
+count = Counter(fish)
+
+#for i in range(1,80):
+for i in range(1,257):
+	dcount=np.zeros(9,dtype=int)
+	for j in (1,2,3,4,5,6,8):
+		dcount[j-1] = count[j]
+	dcount[6] = count[0]+count[7]
+	dcount[8] = count[0]
+	count=dcount
+	print(i,np.sum(count))
