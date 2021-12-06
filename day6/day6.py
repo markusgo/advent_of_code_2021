157cbf738f36c665110bd25c89877a8584a92398
Marcos Gouveia <marcosgouveia13@gmail.com>
2021-12-06 16:29:34 +0000

    day 6 smaller

--- day6/day6.py
+++ day6/day6.py
@@ -1,14 +1,10 @@
-import numpy as np
 from collections import Counter
-fish  = [int(i) for i in open("input","r").read().strip().split(",")]
-count = Counter(fish)
-
-#for i in range(1,80):
+fish  = Counter([int(i) for i in open("input","r").read().strip().split(",")])
 for i in range(1,257):
-	dcount=np.zeros(9,dtype=int)
+	newfish=[0,0,0,0,0,0,0,0,0]
 	for j in (1,2,3,4,5,6,8):
-		dcount[j-1] = count[j]
-	dcount[6] = count[0]+count[7]
-	dcount[8] = count[0]
-	count=dcount
-	print(i,np.sum(count))
+		newfish[j-1] = fish[j]
+	newfish[6] = fish[0]+fish[7]
+	newfish[8] = fish[0]
+	fish=newfish
+print(i,sum(fish))
